47b07b2ea1619ca358299a527c7014f5b93c95f8
bstsgls <bastien.segalas@gmail.com>
2024-11-28 20:45:10 +0100

    Taller_Despliegue

--- myFlaskApp/app_model_hooks.py
+++ myFlaskApp/app_model_hooks.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import numpy as np
 import subprocess
 
-# os.chdir(os.path.dirname(__file__))
+os.chdir(os.path.dirname(__file__))
 
 path_base = "/home/dsonlineli/Taller_Despliegue/"
 
@@ -66,8 +66,8 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
 @app.route('/webhook_2024', methods=['POST'])
 def webhook():
     # Ruta al repositorio donde se realizará el pull
-    path_repo = '/ruta/a/tu/repositorio/en/PythonAnywhere'
-    servidor_web = '/ruta/al/fichero/WSGI/de/configuracion' 
+    path_repo = '/home/bstsgls/myFlaskApp'
+    servidor_web = '/var/www/bstsgls_pythonanywhere_com_wsgi.py' 
 
     # Comprueba si la solicitud POST contiene datos JSON
     if request.is_json:
